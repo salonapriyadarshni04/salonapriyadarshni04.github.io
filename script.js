a30650760659f717d646cc01b13f0da8890b54b2
salonapriyadarshni04 <salonapriyadarshni0412@gmail.com>
2026-04-16 00:28:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Updated Professional Text
-const text = "Full Stack Developer | AI Enthusiast | Future Businesswoman";
+const text = "Software Developer | AI Enthusiast | Future Businesswoman";
 let i = 0;
 
 function type() {
     if (i < text.length) {
         document.getElementById("typing").innerHTML += text.charAt(i);
         i++;
-        setTimeout(type, 80); // Thoda fast typing
+        setTimeout(type, 80);
     }
 }
 
-// Page load hote hi typing start hogi
 window.onload = type;
