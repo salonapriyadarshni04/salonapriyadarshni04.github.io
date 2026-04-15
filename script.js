c23842bcf854beef0398c72dfd59ce71e44c65da
salonapriyadarshni04 <salonapriyadarshni0412@gmail.com>
2026-04-16 00:46:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const text = "Software Developer | AI Enthusiast | Future Businesswoman";
+// Isme Researcher wala role add kar diya hai
+const text = "Full-Stack Developer | Tech Researcher | Future Businesswoman";
 let i = 0;
 
 function type() {
     if (i < text.length) {
         document.getElementById("typing").innerHTML += text.charAt(i);
         i++;
-        setTimeout(type, 80);
+        setTimeout(type, 70);
     }
 }
 
