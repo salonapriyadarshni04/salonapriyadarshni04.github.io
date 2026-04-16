044d725295285dc69dfff473c6803d9b78078836
salonapriyadarshni04 <salonapriyadarshni0412@gmail.com>
2026-04-16 07:20:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const text = "Software Developer | Future Business Leader | Digital Strategist | National Athlete";
+// Typing effect is necessary to fill that blank space you saw earlier!
+const text = "Digital Strategist | Software Developer | Future Business Leader | National Athlete";
 let i = 0;
 
 function type() {
-    const element = document.getElementById("typing");
-    if (element && i < text.length) {
-        element.innerHTML += text.charAt(i);
+    const typingElement = document.getElementById("typing");
+    if (typingElement && i < text.length) {
+        typingElement.innerHTML += text.charAt(i);
         i++;
         setTimeout(type, 75);
     }
 }
 
-window.onload = type;
+// Make sure the script starts after the page loads
+window.onload = function() {
+    type();
+};
